a39e0d249210987ca68677a6ea690a08bf4f5a67
Adolfo <adolfoklk0307@gmail.com>
2025-06-02 22:32:33 -0400

    agregando tailwindcss

--- vite.config.ts
+++ vite.config.ts
@@ -3,16 +3,14 @@
 import legacy from '@vitejs/plugin-legacy'
 import react from '@vitejs/plugin-react'
 import { defineConfig } from 'vite'
+import tailwindcss from '@tailwindcss/vite'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [
-    react(),
-    legacy()
-  ],
+  plugins: [react(), legacy(), tailwindcss()],
   test: {
     globals: true,
     environment: 'jsdom',
-    setupFiles: './src/setupTests.ts',
+    setupFiles: './src/setupTests.ts'
   }
 })
